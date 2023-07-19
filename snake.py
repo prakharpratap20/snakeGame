5cf3df46a25ebe98150b89e917972546ba129248
Prakhar Pratap <prakharpratap2001@gmail.com>
2023-07-19 13:34:31 +0530

    Coding the turtle

---
+++ snake.py
@@ -0,0 +1,17 @@
+# import turtle graphics module
+import turtle
+
+# create a turtle 
+
+my_turtle = turtle.Turtle()
+my_turtle.shape("turtle")
+my_turtle.color("blue")
+
+# giving the turtle commands
+
+my_turtle.forward(100)
+
+
+# to stop the turtle we will have to use the done method 
+
+turtle.done()
