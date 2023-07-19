c9c2748f2d1d462f37b56830a8aba384c633b3de
Prakhar Pratap <prakharpratap2001@gmail.com>
2023-07-19 17:19:35 +0530

    snake can move now

--- basicSnakeMovement.py
+++ basicSnakeMovement.py
@@ -4,13 +4,42 @@ import turtle
 # defining program constants
 WIDTH = 600
 HEIGHT = 600
-DELAY = 400 # milliseconds
+DELAY = 100 # milliseconds
+
+offsets = {
+    "up": (0, 20),
+    "down": (0, -20),
+    "left": (-20, 0),
+    "right": (20, 0)
+}
+
+def go_up():
+    global snake_direction
+    if snake_direction != "down":
+        snake_direction = "up"
+        
+def go_right():
+    global snake_direction
+    if snake_direction != "left":
+        snake_direction = "right"
+        
+def go_down():
+    global snake_direction
+    if snake_direction != "up":
+        snake_direction = "down"
+        
+def go_left():
+    global snake_direction
+    if snake_direction != "right":
+        snake_direction = "left"
+
 
 def move_snake():
     stamper.clearstamps() # remove existing stamps made by stamper
 
     new_head = snake[-1].copy()
-    new_head[0] += 20 
+    new_head[0] += offsets[snake_direction][0]
+    new_head[1] += offsets[snake_direction][1]
     
     # add new head to snake body
     snake.append(new_head)
@@ -37,6 +66,13 @@ screen.title("Snake")
 screen.bgcolor("pink")
 screen.tracer(0) # turn of automatic automation
 
+#Event handlers
+screen.listen()
+screen.onkey(go_up, "Up")
+screen.onkey(go_right, "Right")
+screen.onkey(go_down, "Down")
+screen.onkey(go_left, "Left")
+
 # creating a turtle to do the bidding
 stamper = turtle.Turtle()
 stamper.shape("square")
@@ -44,6 +80,7 @@ stamper.penup()
 
 # creating snake as a list of coordiante pairs
 snake = [[0, 0], [20, 0], [40, 0], [60, 0]]
+snake_direction = "up"
 
 # draw snake for the first time.
 for segment in snake:
